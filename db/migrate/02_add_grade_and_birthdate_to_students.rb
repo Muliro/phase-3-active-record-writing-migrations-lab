bd69c7e2741b2f3fee1569f6630c15abcb9b1111
michael muliro <michael.muliro@student.moringaschool.com>
2022-08-26 11:46:01 +0300

    Done.

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -5,7 +5,7 @@
 
 
 
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[6:1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[6.1]
     def change
         add_column :students, :grade, :integer
         add_column :students, :birthdate, :string
